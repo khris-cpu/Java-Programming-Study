91ec90ee81265a2cbc848c5d358ea44045a8c772
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-08-19 22:56:44 +0700

    Study HelloWorld, Display, Comment, Data Type

---
+++ Java Programming Phase 1/Display.java
@@ -0,0 +1,22 @@
+class Display{
+    public static void main(String[] args) {
+        
+        /* 
+           Display : println vs print  
+        */
+
+        // This is my Comment
+        // Before
+        System.out.println("Before : ");
+        System.out.println("");
+        System.out.println("Welcome to ");
+        System.out.println("Khris Bharmmano");
+        System.out.println("");
+        
+        // After
+        System.out.println("After : ");
+        System.out.println("");
+        System.out.print("Welcom to ");
+        System.out.println("Khris Bharmmano");
+    }
+}
